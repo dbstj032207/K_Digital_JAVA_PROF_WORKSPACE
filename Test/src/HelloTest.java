0d543a378d48383d1f30e80ae650498f00c36df6
younseok <dbs032207@gmail.com>
2022-02-25 18:07:52 +0900

    Commit test

---
+++ Test/src/HelloTest.java
@@ -0,0 +1,12 @@
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+	}
+
+}
